aa0868cf278a90afdeb0e6ca641df46ba9b4bf69
ZhongFuCheng3y <zhongfucheng_123@163.com>
2018-01-12 22:25:15 +0800

    正在编写编辑日志功能，修改网站的样式。现在轮到了编辑页面修改样式。

--- src/main/java/com/zhongfucheng/controller/UploadPicController.java
+++ src/main/java/com/zhongfucheng/controller/UploadPicController.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,8 +22,7 @@ import javax.servlet.http.HttpSession;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by ozc on 2018/1/9.
@@ -54,7 +54,7 @@ public class UploadPicController {
     private ThumbnailService thumbnailService;
 
     /**
-     * 上传图片
+     * 上传头像
      *
      * @param imgsFile
      * @throws IOException
@@ -100,6 +100,58 @@ public class UploadPicController {
 
 
         return ResultUtil.success(new ImagePath(realPath, relativePath));
+
+    }
+
+
+    /**
+     * 上传图片(富文本编辑器)
+     *
+     * @param imgsFile
+     * @throws IOException
+     */
+    @RequestMapping(value = "/upload", produces = {"application/json;charset=UTF-8"})
+    public Map uploadPicFile(@RequestParam MultipartFile imgsFile, HttpSession session) throws IOException {
+
+        //上传文件的名字是不能相同的，因此我们设置一下文件的名称
+        String fileName = new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date());
+        Random random = new Random();
+        for (int i = 0; i < 3; i++) {
+            fileName = fileName + random.nextInt(10);
+        }
+        //拿到该文件的原始名称
+        String originalFilename = imgsFile.getOriginalFilename();
+
+        //获取该文件的后缀
+        String suffix = originalFilename.substring(originalFilename.lastIndexOf("."));
+
+        /***
+         * 绝对路径是留给页面src属性做显示的
+         * 相对路径是保存在数据库中，通过input来进行提交的。
+         */
+        //获得上传文件的绝对路径
+        String realPath = filePath + "/upload/" + fileName + suffix;
+
+        //获得相对路径
+        String relativePath = "/upload/" + fileName + suffix;
+
+        //创建jersy的客户端
+        Client client = Client.create();
+        //创建web资源对象
+        WebResource wr = client.resource(realPath);
+        byte[] bytes = imgsFile.getBytes();
+        //拿到文件的二进制数据，使用web资源对象上传
+        wr.put(bytes);
+
+
+        //返回的JSON要符合富文本编辑器：
+        List<String> list = new ArrayList();
+        list.add(realPath);
+        Map map = new HashMap();
+        map.put("errno", 0);
+        map.put("data", list);
+
+        return map;
 
     }
 }
